6e92e8209e6b3a785050255be67f46fbd82a565a
BYang1234 <33939791+BYang1234@users.noreply.github.com>
2019-06-13 13:01:48 -0400

    Update Geometry_dash.py

--- Geometry_dash.py
+++ Geometry_dash.py
@@ -11,7 +11,7 @@ player_y_reset = 0
 player_x_reset = 0
 player_alive = True
 current_screen = "menu"
-player_loc = [0, 0]
+player_loc = [-100, 0]
 x_rectangle = 100
 
 keys_pressed = {arcade.key.RIGHT: False}
@@ -31,7 +31,7 @@ def setup():
     arcade.run()
 
 def update(delta_time):
-    global space_clicked, space_released, player_y_reset, player_loc, keys_pressed
+    global space_clicked, space_released, player_y_reset, player_loc, keys_pressed, player_alive
     if keys_pressed[arcade.key.RIGHT]:
         player_loc[0] += 4
     if space_clicked == True:
@@ -40,12 +40,12 @@ def update(delta_time):
     elif space_clicked == False:
         player_loc[1] = player_y_reset
 
-    for i in range(10):
+    for i in range(1):
         x = x_rectangle + i * 300
-        y = 0
+        y = -20
         w = 50
         h = 70
-        if player_loc[0] >= x and player_loc[0] + w and player_loc[1] <= y + w and player_loc[1] >= y:
+        if player_loc[0] >= x and player_loc[0] <= x + w and player_loc[1] <= h:
             player_alive = False
             player_loc[0] = player_x_reset
 
@@ -108,12 +108,12 @@ def play_screen():
     arcade.set_background_color(arcade.color.SKY_BLUE)
     arcade.draw_text("GEOMETRY DASH", WIDTH / 2 - 160, 400, arcade.color.BLACK, 30)
     arcade.draw_rectangle_filled(player_loc[0] + 33, player_loc[1] + 23, 60, 60, arcade.color.YELLOW)
-    for i in range(10):
+    for i in range(1):
         x = x_rectangle + i*300
-        y = 0
+        y = -20
         w = 50
         h =70
-        arcade.draw_rectangle_filled(x, y, w, h, arcade.color.BLACK)
+        arcade.draw_xywh_rectangle_filled(x, y, w, h, arcade.color.BLACK)
 
     arcade.draw_text("ESC to go back", 540, 400, arcade.color.BLACK, 10)
     arcade.set_viewport(-WIDTH / 2 + player_loc[0] + 320,
